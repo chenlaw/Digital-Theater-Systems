8a5f69ff2411cc63ef17f9cd9d08a81c5d28e425
kennyfortune <370774330@qq.com>
2019-06-01 18:38:47 +0800

    补充人员管理html

---
+++ 4f6735326a794978bd4df92fe95121de/src/main/resources/static/js/adminStaffManage.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    getAllUser();
+    function getAllUser() {
+        
+    }
+    $("#getOneUser").click(function () {
+
+    });
+    $("#add-btn").click(function(){
+        
+    });
+    $("#modify-btn").click(function () {
+
+    });
+    $("#delete-btn").click(function () {
+
+    });
+});
