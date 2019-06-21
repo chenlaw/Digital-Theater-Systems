999dee16cd56c8f35a436b7a08c543c583fa93ac
chenlaw <171250543@smail.nju.edu.cn>
2019-06-21 09:22:04 +0800

    test

---
+++ 4f6735326a794978bd4df92fe95121de/src/test/java/com/example/cinema/blImpl/record/ConsumptionImplTest.java
@@ -0,0 +1,34 @@
+package java.com.example.cinema.blImpl.record;
+
+import com.example.cinema.CinemaApplication;
+import com.example.cinema.bl.record.ConsumptionService;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = CinemaApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@Transactional
+public class ConsumptionImplTest {
+    @Autowired
+    ConsumptionService consumptionService;
+    @Test
+    public void recordConsumption(){
+        Assert.assertEquals(true,consumptionService.recordConsumption(ConsumptionStub.insertConsumption()));
+    }
+    @Test
+    public void getConsumptionByBalanceTest(){
+        Assert.assertEquals(true,consumptionService.getConsumtionsOrderByBalance(34));
+    }
+    @Test
+    public void getConsumptionByIdTest(){
+        Assert.assertEquals(true,consumptionService.getConsumtions("3"));
+    }
+}
